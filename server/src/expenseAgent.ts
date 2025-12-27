a4eb452e567f5164b8e31718b12b5bec38666947
Vinayaka Swamy H M <86816121+VinZCodz@users.noreply.github.com>
2025-12-27 15:59:03 +0000

    proper DI for agentic graph to make it ready for llm testing

---
+++ server/src/expenseAgent.ts
@@ -0,0 +1,41 @@
+import { StateGraph, MemorySaver } from "@langchain/langgraph";
+import { ToolNode } from "@langchain/langgraph/prebuilt";
+import { AIMessage } from "@langchain/core/messages";
+import { MessagesState } from "./state";
+import type { BaseChatModel } from "@langchain/core/language_models/chat_models";
+import type { StructuredTool } from "langchain";
+
+export const createExpenseTracker = ({ llm, tools, systemPrompt, checkpointer }: AgentDependencies) => {
+    const callModel = async (state: typeof MessagesState.State) => {
+        const llmWithTools = llm.bindTools!(tools);
+
+        const response = await llmWithTools.invoke([
+            { role: "system", content: systemPrompt },
+            ...state.messages
+        ]);
+
+        return { messages: [response] };
+    };
+
+    const toolNode = new ToolNode(tools);
+
+    const shouldContinue = (state: typeof MessagesState.State) => {
+        const lastMessage = state.messages.at(-1) as AIMessage;
+        return (lastMessage?.tool_calls?.length ?? 0) > 0 ? "toolNode" : "__end__";
+    };
+
+    return new StateGraph(MessagesState)
+        .addNode("callModel", callModel)
+        .addNode("toolNode", toolNode)
+        .addEdge("__start__", "callModel")
+        .addConditionalEdges("callModel", shouldContinue)
+        .addEdge("toolNode", "callModel")
+        .compile({ checkpointer });
+};
+
+export type AgentDependencies = {
+    llm: BaseChatModel;
+    tools: StructuredTool[];
+    systemPrompt: string;
+    checkpointer: MemorySaver;
+};
--- server/test/consoleChat.ts
+++ server/test/consoleChat.ts
@@ -1,6 +1,23 @@
 import * as readline from 'node:readline/promises';
-import { ExpenseTrackerAgent } from "../src/graph.ts";
+import { MemorySaver } from '@langchain/langgraph';
+import { createExpenseTracker } from "../src/expenseAgent.ts";
+import { ExpenseService } from "../src/services/ExpenseService.ts";
+import * as client from "../src/db/client.ts";
+import type { LibSQLDatabase } from 'drizzle-orm/libsql';
+import * as expense from '../src/db/schema/expenses.ts';
+import { SelectQueryGuard } from "../src/utils/SelectQueryGuard.ts";
+import { initTools } from "../src/tools.ts";
+import { llm } from "../src/model.ts"
+import { SYSTEM_PROMPT } from "../src/prompt.ts"
 
+const expenseService = new ExpenseService(
+    client.db as unknown as LibSQLDatabase<typeof expense>,
+    client.roDB as unknown as LibSQLDatabase<typeof expense>,
+    new SelectQueryGuard()
+);
+const tools = initTools(expenseService)
+
+const ExpenseTrackerAgent = createExpenseTracker({ llm, tools, systemPrompt: SYSTEM_PROMPT, checkpointer: new MemorySaver() });
 const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
 const main = async () => {
