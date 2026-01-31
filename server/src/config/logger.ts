652bf7a8b04d29f6f4fff7eda245d740b9101b37
Vinayaka Swamy H M <86816121+VinZCodz@users.noreply.github.com>
2026-01-31 04:52:16 +0000

    winston logger: setup with sync and cross env

---
+++ server/src/config/logger.ts
@@ -0,0 +1,32 @@
+import winston from 'winston'
+
+const logger = winston.createLogger({
+    level: 'info',
+    defaultMeta: {
+        serviceName: 'expense_agent',
+    },
+    format: winston.format.combine(
+        winston.format.timestamp(),
+        winston.format.json()
+    ),
+    transports: [
+        new winston.transports.Console({
+            level: 'debug',
+            silent: process.env.NODE_ENV === 'prod',
+        }),
+        new winston.transports.File({
+            level: 'error',
+            dirname: 'logs',
+            filename: 'error.log',
+            silent: process.env.NODE_ENV === 'dev',
+        }),
+        new winston.transports.File({
+            level: 'info',
+            dirname: 'logs',
+            filename: 'combined.log',
+            silent: process.env.NODE_ENV === 'dev',
+        }),
+    ],
+})
+
+export default logger
